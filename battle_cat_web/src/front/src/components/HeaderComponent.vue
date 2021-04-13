6488ddf523e370e7f2d92c5e1cf0913c19ecaa70
gujerbit <ghftm1124@gmail.com>
2021-04-13 10:58:53 +0900

    header 거의 완성 (2021-04-13)

--- battle_cat_web/src/front/src/components/HeaderComponent.vue
+++ battle_cat_web/src/front/src/components/HeaderComponent.vue
@@ -5,7 +5,32 @@
                 <img src="/../../imgs/temp/header_logo.png" alt="">
             </div>
             <div class="header-menu">
-                
+                <nav class="menu-bar">
+                    <ul class="main-menu-bar">
+                        <li>
+                            <router-link to="">아군 캐릭터 정보</router-link>
+                        </li>
+                        <li>
+                            <router-link to="">적군 캐릭터 정보</router-link>
+                        </li>
+                        <li>
+                            <router-link to="">스테이지 정보</router-link>
+                        </li>
+                        <li>
+                            <router-link to="">기타 정보</router-link>
+                        </li>
+                        <li>
+                            <router-link to="">커뮤니티</router-link>
+                        </li>
+                        <li>
+                            <router-link to="">정보/유저 검색</router-link>
+                        </li>
+                    </ul>
+                </nav>
+            </div>
+            <div class="header-user">
+                <router-link to="">로그인</router-link>
+                <router-link to="">회원가입</router-link>
             </div>
         </div>
     </header>    
--- battle_cat_web/src/front/src/components/MainComponent.vue
+++ battle_cat_web/src/front/src/components/MainComponent.vue
@@ -1,6 +1,17 @@
 <template>
     <main>
-        
+        <div class="main-container">
+            <div class="main-banner">
+                <div class="banner-imgs">
+                    <img src="/../../imgs/res/event/event_banner_easter.png" alt="">
+                </div>
+            </div>
+            <div class="main-contents">
+                <div class="main-schedule">
+                    
+                </div>
+            </div>
+        </div>
     </main>
 </template>
 
