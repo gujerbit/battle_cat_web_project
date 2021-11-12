095ea39982c4e70b918288d75769951f102c3e17
gujerbit <ghftm1124@gmail.com>
2021-11-12 17:57:16 +0900

    swal 도입중/swal 페이지 로드 막아야하고 크기 키워야함 (2021-11-12)

--- battle_cat_web/src/front/src/components/community/CommunityComponent.vue
+++ battle_cat_web/src/front/src/components/community/CommunityComponent.vue
@@ -1,7 +1,6 @@
 <template>
   <main>
     <div class="wrap">
-      this system 1.2 version available
       <button @click="logout()">logout</button>
     </div>
     <router-link to="/" class="main-page">메인 화면으로 돌아가기</router-link>
@@ -9,21 +8,34 @@
 </template>
 
 <script>
-import { onBeforeMount } from 'vue';
+import { onBeforeMount, getCurrentInstance } from 'vue';
+import { useRouter } from 'vue-router';
 
 export default {
   setup() {
+    const { proxy } = getCurrentInstance();
+    const router = useRouter();
+
     const logout = () => {
       alert('로그아웃 완료');
       window.sessionStorage.removeItem('jwt-auth-token');
       window.sessionStorage.removeItem('user-info');
       location.href = '/login';
-    }
+    };
 
     onBeforeMount(() => {
       if(window.sessionStorage.getItem('jwt-auth-token') === null) {
-        alert('로그인 후 이용가능한 시스템입니다');
-        location.href = '/login';
+        proxy.Swal.fire({
+          icon: 'warning',
+          titleText: '잘못된 접근',
+          text: '로그인 후 이용가능한 시스템입니다',
+          heightAuto: false,
+          willOpen: () => {
+            window.stop();
+          }
+        }).then(res => {
+          if(res) router.push('/login');
+        });
       }
     });
 
--- battle_cat_web/src/front/src/main.js
+++ battle_cat_web/src/front/src/main.js
@@ -3,11 +3,13 @@ import App from './App.vue';
 import router from './router';
 import store from './store';
 import axios from 'axios';
+import Swal from 'sweetalert2';
 
 const app = createApp(App);
 axios.defaults.baseURL = 'https://localhost';
 // axios.defaults.baseURL = 'https://nyanko-db.shop';
 app.config.globalProperties.axios = axios;
 app.config.globalProperties.store = store;
+app.config.globalProperties.Swal = Swal;
 app.use(router).use(store).mount('#app');
 //createApp(App).use(router).mount('#app');
