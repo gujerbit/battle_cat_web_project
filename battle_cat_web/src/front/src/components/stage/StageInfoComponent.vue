f1173bd41e43c28a042c0dbe77517f073fed1e66
gujerbit <ghftm1124@gmail.com>
2022-01-10 18:00:31 +0900

    스테이지 정보 페이지 메뉴 제작 중 (2022-01-10)

--- battle_cat_web/src/front/src/components/stage/StageInfoComponent.vue
+++ battle_cat_web/src/front/src/components/stage/StageInfoComponent.vue
@@ -4,7 +4,12 @@
       <stage-info-element-component />
       <section>
         <div class="content">
-          {{info.all}}
+          {{info.current}}
+        </div>
+        <div class="pages">
+          <p class="prev" @click="prevPage()">&lt;</p>
+          <p :class="'page ' + `${pageInfo.currentPage === value ? 'select' : ''}`" v-for="value in pageInfo.totalPage[pageInfo.viewPage]" :key="value" @click="selectPage(value)">{{value}}</p>
+          <p class="next" @click="nextPage()">&gt;</p>
         </div>
       </section>
     </div>
@@ -17,6 +22,7 @@ import { ref, getCurrentInstance, onBeforeMount } from 'vue';
 
 import stageInfoElementComponent from './stageInfoElementComponent.vue';
 import { getStageInfo } from '../../js/stage/stageInfo.js';
+import { pagination, pageDivision } from '../../js/util/pagination.js';
 
 export default {
   setup() {
@@ -27,12 +33,46 @@ export default {
       current: [],
     });
 
+    const pageInfo = ref({
+      totalPage: [], //전체 페이지
+      divisionPage: 36, //한 번호에 몇 개씩 데이터를 보여줄 것인지 정하는 변수
+      currentPage: 1, //현재 페이지
+      viewPage: 0, //현재 페이지 배열의 index
+    });
+    //다음 페이지 index
+    const nextPage = () => {
+      if(pageInfo.value.totalPage.length - 1 > pageInfo.value.viewPage) { //현재 페이지 배열의 인덱스가 전체 페이지 배열의 크기 보다 작다면
+        pageInfo.value.viewPage++; //현재 페이지 배열의 index 증가
+        pageInfo.value.currentPage = pageInfo.value.totalPage[pageInfo.value.viewPage][0]; //해당 페이지 배열의 index에 있는 번호 중 가장 앞 번호를 현재 번호로 설정
+        contentUpdate(); //현재 번호에 해당하는 내용 업데이트
+      }
+    };
+    //이전 페이지 index
+    const prevPage = () => {
+      if(0 < pageInfo.value.viewPage) { //현재 페이지 배열의 index가 0보다 크다면
+        pageInfo.value.viewPage--; //현재 페이지 배열의 index 감소
+        pageInfo.value.currentPage = pageInfo.value.totalPage[pageInfo.value.viewPage][0]; //해당 페이지 배열의 index에 있는 번호 중 가장 앞 번호를 현재 번호로 설정
+        contentUpdate(); //현재 번호에 해당하는 내용 업데이트
+      }
+    };
+    //현재 번호 설정
+    const selectPage = (page) => {
+      pageInfo.value.currentPage = page; //번호 설정
+      contentUpdate(); //현재 번호에 해당하는 내용 업데이트
+    };
+    //현재 번호에 해당하는 내용 업데이트
+    const contentUpdate = () => {
+      info.value.current = pagination(info.value.all, pageInfo.value.currentPage, pageInfo.value.divisionPage); //현재 번호에 해당하는 내용 페이지네이션
+      pageInfo.value.totalPage = pageDivision(info.value.all, pageInfo.value.divisionPage); //페이지 배열 설정
+    };
+
     onBeforeMount(() => {
       proxy.store.commit('setSearchStageInfo', []);
       info.value.all = getStageInfo(proxy.store);
+      contentUpdate();
     });
 
-    return { info };
+    return { info, pageInfo, nextPage, prevPage, selectPage };
   },
   components: {
     stageInfoElementComponent
@@ -62,11 +102,48 @@ section {
   width: 90%;
   height: 100%;
   margin: 0 auto;
+  display: grid;
+  grid-template-rows: 94.5% 4.5%;
 }
 
 .content {
   width: 100%;
   height: 100%;
+}
+
+.pages {
+  width: 100%;
+  height: 100%;
+  margin: 0 auto;
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  border: 1.5px solid #ffc038;
+}
+  
+.pages p {
+  width: 2.5%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  cursor: pointer;
+}
+  
+.page {
+  width: 100%;
+  height: 100%;
+  font-size: 2.8rem;
+}
+  
+.select {
+  color: #ffc038;
+  pointer-events: none;
+}
+  
+.prev, .next {
+  width: 100%;
+  height: 100%;
+  font-size: 2.8rem;
 }
 
 .main-page {
--- battle_cat_web/src/front/src/components/stage/stageInfoElementComponent.vue
+++ battle_cat_web/src/front/src/components/stage/stageInfoElementComponent.vue
@@ -2,10 +2,10 @@
   <div id="element">
     <div class="content">
       <ul class="main">
-        <li @mouseover="subMenuShowTrigger.show[idx] = true; subMenuShowTrigger.render = true;" @mouseleave="subMenuShowTrigger.show[idx] = false;" v-for="(value, idx) in mainMenus" :key="value">{{value}}</li>
+        <li :class="searchInfo.search.split('_')[0] === value.split('/')[1] ? 'select' : ''" @click="selectMenu(idx, index)" @mouseover="subMenuShowTrigger.show[idx] = true; subMenuShowTrigger.render = true;" @mouseleave="subMenuShowTrigger.show[idx] = false;" v-for="(value, idx) in mainMenus" :key="value">{{value.split('/')[0]}}</li>
         <div class="subs" v-if="subMenuShowTrigger.render">
           <ul @mouseover="subMenuShowTrigger.show[idx] = true; subMenuShowTrigger.render = true;" @mouseleave="subMenuShowTrigger.show[idx] = false; subMenuShowTrigger.render = false;" class="sub" v-for="(value, idx) in subMenus" :key="value">
-            <li v-show="subMenuShowTrigger.show[idx]" v-for="item in value" :key="item">{{item}}</li>
+            <li @click="selectMenu(idx, index)" v-show="subMenuShowTrigger.show[idx]" v-for="(item, index) in value" :key="item">{{item.split('/')[0]}}</li>
           </ul>
         </div>
       </ul>
@@ -18,23 +18,35 @@ import { ref } from 'vue';
 
 export default {
   setup() {
-    const mainMenus = ['세계편', '미래편', '우주편', '마계편', '레전드 스토리', '신레전드 스토리', '스페셜 스테이지', '콜라보 스테이지'];
+    const mainMenus = ['세계편/world', '미래편/future', '우주편/space', '마계편/hell', '레전드 스토리/legend', '신레전드 스토리/origin_legend', '스페셜 스테이지/special', '콜라보 스테이지/collaboration'];
     const subMenus = [
-      ['메인', '좀비 습격'],
-      ['메인', '좀비 습격'],
+      ['메인/main', '좀비 습격/zombie'],
+      ['메인/main', '좀비 습격/zombie'],
       [],
       [],
       [],
       [],
-      ['게릴라', '요일', '월간', '드롭', '각성', '발굴', '이벤트', '기타'],
+      ['게릴라/guerrilla', '요일/week', '월간/month', '드롭/drop', '각성/arousal', '발굴/excavation', '이벤트/event', '기타/etc'],
       [],
     ];
     let subMenuShowTrigger = ref({
       show: [false, false, false, false, false, false, false, false],
       render: false,
     });
+    let searchInfo = ref({
+      search: '',
+    });
 
-    return { mainMenus, subMenus, subMenuShowTrigger, };
+    const selectMenu = (main, sub) => {
+      searchInfo.value.search = mainMenus[main].split('/')[1];
+
+      if(subMenus[main].length > 0) {
+        if(sub !== undefined) searchInfo.value.search += ('_' + subMenus[main][sub].split('/')[1]);
+        else return;
+      }
+    };
+
+    return { mainMenus, subMenus, subMenuShowTrigger, searchInfo, selectMenu };
   }
 }
 </script>
@@ -102,5 +114,9 @@ export default {
 .sub > li:hover {
   padding-top: 1.5px;
   border-bottom: 1.5px solid #ffc038;
+}
+
+.select {
+  color: #ffc038;
 }
 </style>
